7676dd2f88932ec66075fd3671432643e23a0639
wayne <wayne@che.ng>
2017-05-13 21:33:21 -0700

    beta testing

--- assets/js/api-wayne.js
+++ assets/js/api-wayne.js
@@ -1,70 +1,59 @@
 
 
 
+// var uIP, uBrowser;
+// var q;
+
+//  Click Event
+// $('#submit').on('click', function(){
+//     event.preventDefault();
+// 	q = $('#search').val();
+
+//     gd();
+// });
+
+
+	// $.getJSON('https://jsonip.com/?callback=?', function (data) {
+	// 	uIP = data.ip;
+	// });
+ //   	uBrowser = bowser.name;
 $(document).ready(function(){
 
 var uIP, uBrowser;
-var q;
 
-//  Click Event
-$('#submit').on('click', function(){
-    event.preventDefault();
-	q = $('#search').val();
+	// $.getJSON('https://jsonip.com/?callback=?', function (data) {
+	// 	var uIP = data.ip;
+	// });
 
-    getGlassDoor();
-});
-
-// var userInfo = {
-// 					'ip': '',
-// 					'browser': '',
-// 					'version': bowser.version,
-// 				};
-
-	$.getJSON('https://jsonip.com/?callback=?', function (data) {
-		uIP = data.ip;
-	});
-    // User Bowser to get Browser/userAgent. 
-   	uBrowser = bowser.name;
-
-    // navigator.userAgent is widely discouraged, due to ineffectiveness
-    // var agent = navigator.userAgent;
+		$.getJSON("http://api.glassdoor.com/api/api.htm", {
+		    v: 1,
+		    format: 'json',
+		    't.p': '151095',
+		    't.k': 'dSWk91gUjq3',
+		    action: 'employers',
+		    q: 'developer',
+		    userip: '0.0.0.0',
+		    useragent: bowser.name
+		}, function(response) {
+		    console.log('response',response);
+		});
 
 
-function getGlassDoor(){
-
-			// var gd_tp, gd_tk, gd_userIP, gd_action;
-			// gd_tp = '151095';
-			// gd_tk = 'dSWk91gUjq3';
-			// gd_userIP = uIP;
-			// gd_userAgent = userInfo.browser +'/%2F'+ userInfo.version;
-			// gd_action = 'employers';
-			// console.log('gd_userIP',gd_userIP);
+// };
+// gd();
+})
+			// $.ajax({
+			// 	type: 'GET',
+			// 	url: gd_queryURL,
+			// }).done(function(response){
+			// 	console.log('response',response);
 
 
 
-			// var gd_queryURL = 'http://api.glassdoor.com/api/api.htm?v=1&format=json&t.p='+ gd_tp +'&t.k='+ gd_tk +'&action='+ gd_action +'&q=pharmaceuticals&userip='+ uIP +'&useragent='+ api.userInfo.browser +'/%2F'+ api.userInfo.version;
-			// var gd_queryURL = 'http://api.glassdoor.com/api/api.htm?v=1&format=json&t.p=151095&t.k=dSWk91gUjq3&action=employers&q='+ q +'&userip='+ userInfo.ip +'&useragent='+ gd_userAgent;
+			// }); //end ajax
 
-			// var gd_queryURL = 'http://api.glassdoor.com/api/api.htm?v=1&format=json&t.p=151095&t.k=dSWk91gUjq3&action=employers&q=developer&userip=2602:306:c40d:54b0:45fe:eeaf:3f0c:b0bd&useragent=Chrome';
-			var gd_queryURL = 'https://api.glassdoor.com/api/api.htm?v=1&format=json&t.p=151095&t.k=dSWk91gUjq3&action=employers&q='+ q +'&userip='+ uIP +'&useragent='+ uBrowser;
-
-
-			console.log('gd_queryURL',gd_queryURL);
-
-
-			$.ajax({
-				type: 'GET',
-				url: gd_queryURL
-			}).done(function(response){
-				console.log('response',response);
-
-
-
-			}); //end ajax
-
-		}
+		
 
 
 
 
-});
