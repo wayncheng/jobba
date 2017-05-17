dcca4c2ddc264d8c0daaea56aa481a20da2b4b4a
novialim <novialim@gmail.com>
2017-05-17 09:40:52 -0700

    Added s in wayne code

--- assets/js/api-wayne.js
+++ assets/js/api-wayne.js
@@ -31,7 +31,7 @@ $(document).ready(function(){
 	//UNCOMMENTED WAYNE'S CODE
 
 	var uIP, uBrowser;
-	var qURL = 'http://api.glassdoor.com/api/api.htm?v=1&format=json&t.p=151095&t.k=dSWk91gUjq3&action=employers&q=web&userip=192.185.16.105&useragent=Chrome';
+	var qURL = 'https://api.glassdoor.com/api/api.htm?v=1&format=json&t.p=151095&t.k=dSWk91gUjq3&action=employers&q=web&userip=192.185.16.105&useragent=Chrome';
 	
 	$.ajax({
 		type:'GET',
