635b9ead34f96b4f14245c0a73b91b0d9887095b
novialim <novialim@gmail.com>
2017-06-01 00:09:58 -0700

    mini cleanup

--- assets/js/removejob.js
+++ assets/js/removejob.js
@@ -1,8 +1,6 @@
 
 	$("#saved-jobs-modal").on("click", ".remove-btn", function() {
 		var jobKey = $(this).parent().parent().attr('id');
-			console.log("helloooo:"+jobKey);
-			console.log("is user still on??"+userId);
 			database.ref("/"+userId+"/jobs/"+jobKey).remove();
 
 	});
