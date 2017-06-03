d98336d03f63f430fca97e2899e8b71b0e6cec46
wayne <wayne@che.ng>
2017-06-03 01:51:43 -0700

    Last search local bug fix. Added location icon to location input

--- assets/js/filter-sort.js
+++ assets/js/filter-sort.js
@@ -5,10 +5,10 @@
 
 
 // Filter button
-	$('#applyFilters').on('click',function(e){
-		e.preventDefault();
-		jobba.filter.byTerms.fx(whiteList,blackList);
-	})
+	// $('#applyFilters').on('click',function(e){
+	// 	e.preventDefault();
+	// 	jobba.filter.byTerms.fx(whiteList,blackList);
+	// })
 // Trigger filter on focus out
 	// $('#filter-side-nav').on('focusout',function(e){
 	// 	e.preventDefault();
