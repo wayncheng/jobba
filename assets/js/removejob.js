86653ab49836d3f6c32c522651f9f5ef6a62fafd
Wayne Cheng <wayne@che.ng>
2017-06-05 19:06:19 -0700

    remove job fix

--- assets/js/removejob.js
+++ assets/js/removejob.js
@@ -2,8 +2,14 @@
 	$("#saved-jobs-modal").on("click", ".remove-btn", function() {
 		var jobKey = $(this).parent().parent().attr('id');
 			database.ref("/"+userId+"/jobs/"+jobKey).remove();
+		var feedSel = '#feed [data-source-id="'+jobKey+'"]';
+		var saveWrap = $(feedSel).find('.save-wrap');
+			saveWrap.removeClass('saved');
+			saveWrap.attr('data-saved','true');
+			
 
-			console.log("The key of job is?" +jobKey);
+
+		console.log("The key of job is?" +jobKey);
 
 	});
 
